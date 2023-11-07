40591417deab6f483f314cc0ef1716682755b1cc
Yvo Keller <keller.yvo@gmail.com>
2023-11-07 10:33:41 +0100

    remove distrubution restriction on dataset

--- build_dataset.py
+++ build_dataset.py
@@ -4,13 +4,14 @@ The split is done with respect to the number of words in the 'text' column and c
 The concern part of the dataset is derived from a english dataset by translation. Thats why the execution of the script can take a couple of minutes.
 """
 
-DEVICE='mps'
+DEVICE='auto'
 
 TRAIN_SIZE=0.8 #size of training set
 SEED=420 #seed for train test split
 MIN_WORD_COUNT=3 #how many words at least must be present in a text to be included
 MAX_WORD_COUNT=13 #how many words at max are allowed to be in a text to be included
 BINS=5 #number of bins for alignment (the more bins the more accurately are the individual dataframes sampled according to word count) (BINS is not allowed to be bigger than [MAX_WORD_COUNT - MIN_WORD_COUNT], in this case each bin gets a single word length)
+WORD_DIST_ADJUST=False # true -> tries to keep the word count distribution same for all classes
 
 from datasets import load_dataset
 import pandas as pd
@@ -77,38 +78,48 @@ def load_concern_dataset():
     os.remove(filename)
 
     #filter for "School"
-    concern = data[data.top_label == "School"][["sentence"]].reset_index(drop=True, inplace=False)
+    concern = data[data.top_label.isin(["School", "Work", "Family Issues"])][["sentence"]].reset_index(drop=True, inplace=False)
     concern.columns = ["text"] #rename
 
     return concern
 
-def sampler(dfs:list, min_max_bins:tuple, train_split:float=0.8, seed=None):
-    bins = pd.interval_range(start=min_max_bins[0], end=min_max_bins[1], freq=None, periods=min_max_bins[2], closed='right')
-    intervals_list = [pd.cut(df.text.apply(lambda x: len(x.split())), bins) for df in dfs]
+def sampler(dfs:list, train_split:float=0.8, seed=None, use_bins:bool=True, min_max_bins:tuple=None):
+    # Calculate the minimum count across all dataframes
+    min_count = min(df.shape[0] for df in dfs)
 
-    # Calculate the minimum counts for each interval across all dataframes
-    min_counts = intervals_list[0].value_counts()
-    for intervals in intervals_list[1:]:
-        current_counts = intervals.value_counts()
-        min_counts = pd.concat([min_counts, current_counts], axis=1).min(axis=1)
-
-    # Group each dataframe by their intervals
-    grouped_list = [df.groupby(intervals, observed=True) for df, intervals in zip(dfs, intervals_list)]
-    
     results = []
-    for df, df_grouped in zip(dfs, grouped_list):
+    for df in dfs:
         df_sampled_train = []
         df_sampled_test = []
 
-        for interval, min_count in min_counts.items():
-            if interval in df_grouped.groups:
-                df_sampled = df_grouped.get_group(interval).sample(n=min_count, random_state=seed)
+        if use_bins and min_max_bins is not None:
+            # Create bins and bin the data
+            bins = pd.interval_range(start=min_max_bins[0], end=min_max_bins[1], periods=min_max_bins[2], closed='right')
+            intervals = pd.cut(df.text.apply(lambda x: len(x.split())), bins)
 
-                df_train = df_sampled.sample(frac=train_split, random_state=seed)
-                df_test = df_sampled.drop(df_train.index)
+            # Group dataframe by intervals
+            df_grouped = df.groupby(intervals, observed=True)
 
-                df_sampled_train.append(df_train)
-                df_sampled_test.append(df_test)
+            for interval in bins:
+                if interval in df_grouped.groups:
+                    df_interval_group = df_grouped.get_group(interval)
+                    # Ensure we don't try to sample more items than exist in the smallest interval group
+                    interval_min_count = min(min_count, df_interval_group.shape[0])
+                    df_sampled = df_interval_group.sample(n=interval_min_count, random_state=seed)
+
+                    df_train = df_sampled.sample(frac=train_split, random_state=seed)
+                    df_test = df_sampled.drop(df_train.index)
+
+                    df_sampled_train.append(df_train)
+                    df_sampled_test.append(df_test)
+        else:
+            # Sample without binning
+            df_sampled = df.sample(n=min_count, random_state=seed)
+            df_train = df_sampled.sample(frac=train_split, random_state=seed)
+            df_test = df_sampled.drop(df_train.index)
+
+            df_sampled_train.append(df_train)
+            df_sampled_test.append(df_test)
 
         results.append((pd.concat(df_sampled_train, ignore_index=True), pd.concat(df_sampled_test, ignore_index=True)))
 
@@ -124,7 +135,6 @@ class Translate_EN_DE():
         # Model: https://huggingface.co/docs/transformers/model_doc/nllb & https://huggingface.co/facebook/nllb-200-distilled-1.3B
         # This model performed best in comparison with (https://huggingface.co/facebook/nllb-200-distilled-600M, https://huggingface.co/t5-base, https://huggingface.co/facebook/mbart-large-50-many-to-many-mmt) in a qualitative view
         # Model Size is also restrited due to limiations of gpu memory
-        self.device = device
         self.max_length = max_length
         self.tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-1.3B", fast=True)
         self.model:M2M100ForConditionalGeneration = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-1.3B", device_map=device)
@@ -140,7 +150,7 @@ class Translate_EN_DE():
         """
 
         def translate_batch(data:list):
-            inputs = self.tokenizer(data, padding=True, return_tensors="pt").to(self.device)
+            inputs = self.tokenizer(data, padding=True, return_tensors="pt").to(self.model.device)
 
             translated_tokens = self.model.generate(
                 **inputs, 
@@ -164,7 +174,7 @@ if __name__ == "__main__":
     concern["text"] = Translate_EN_DE(DEVICE).translate(concern.text.to_list())
 
     #sample datasets
-    (sampled_questions_train, sampled_questions_test), (sampled_offense_train, sampled_offense_test), (sampled_concern_train, sampled_concern_test) = sampler([questions, offensive, concern], (MIN_WORD_COUNT, MAX_WORD_COUNT, BINS), TRAIN_SIZE, SEED)
+    (sampled_questions_train, sampled_questions_test), (sampled_offense_train, sampled_offense_test), (sampled_concern_train, sampled_concern_test) = sampler(dfs=[questions, offensive, concern], min_max_bins=(MIN_WORD_COUNT, MAX_WORD_COUNT, BINS), train_split=TRAIN_SIZE, seed=SEED, use_bins=WORD_DIST_ADJUST)
 
     #assign label to questions
     sampled_questions_train["label"] = "question"
